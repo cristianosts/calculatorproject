747c978b14a7c387fe263cdad3620ad507ad962a
cristianosts <cristianosantosreal@gmail.com>
2024-11-29 21:53:38 -0300

    Update act.js

--- act.js
+++ act.js
@@ -35,6 +35,18 @@ class Calculator {
         operationValue = previous + current
         this.updateScreen(operationValue, operation, current, previous)
         break
+      case '+':
+        operationValue = previous + current
+        this.updateScreen(operationValue, operation, current, previous)
+       break
+      case '+':
+        operationValue = previous + current
+        this.updateScreen(operationValue, operation, current, previous)
+       break
+      case '+':
+        operationValue = previous + current
+        this.updateScreen(operationValue, operation, current, previous)
+       break
       default:
         return
     }
